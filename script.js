c962d2ee87cee3327575d8689889e1035e9b628a
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-01-04 13:02:48 -0500

    more expl

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ var doWork = function (){
 
 //console.log(doWork());  // Objectfn: "srinivas"ln: "pv"twitter: "srini88"__proto__: Object
 
-let {fn:fn, ln:ln, twitter:twitter} = doWork();
+let {fn:fn, twitter:twitter} = doWork();
+
+
+
 
 // above is not building an obj literal
 // why object declaration then ?
 // assigning values that is returned from object thats why {} because right hand side
 // is returning an object
-console.log(fn, ln, twitter);  // srinivas pv srini88
-// fn , ln and twitter are godammn variables
+console.log(fn,  twitter);  // srinivas srini88
