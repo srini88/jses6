41c940c6e33e9631b1d8d3a03a1b32967718b0bf
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-01-04 11:54:51 -0500

    const is readonly, cannot change value

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var doWork = function(flag){
-  for (var i=0; i<10; ++i){
-  }
-  return i;
+// varibale that will hold constant value (read only)
+//has block level scoping
+const MAX_SIZE = 10;
+
+//MAX_SIZE = 20;
+
+//console.log(MAX_SIZE); //Uncaught TypeError: Assignment to constant variable.
+// semantics of true es6 const , ecma6 it is a syntax error
+
+
+//const does have block semantics
+var doWork = function(){
+  var x = 12;
+  var x = 10;  //this one wins and overrides
+  return x;
 }
 
-console.log(doWork());  //10 //we get 10 because i is hoisted, it is not only
-//within the for which we wanted
-
-var doWorkLet = function(flag){
-  for (let i=0; i<10; ++i){
-  }
-  return i;
-}
-
-console.log(doWorkLet());  //error - this i is constrained within for and we get
-//true block scoping
+console.log(doWork());  //10
