719134703729ba9be4f343ecd07cecfe11269e3b
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-01-04 13:04:49 -0500

    awesome expl

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ var doWork = function (){
 
 //console.log(doWork());  // Objectfn: "srinivas"ln: "pv"twitter: "srini88"__proto__: Object
 
-let {fn:fn, twitter:twitter} = doWork();
 
+let {fn:first, twitter:twitter} = doWork();
+// above // fn property from the object returned from doWork() and assign that
+// shit to 'first' property
 
+// the below shit works
+// take away , the first property is the one returned
+// the second one after colon is any shit variable name, i used first which is not
+// matching with fn, still that shit works
 
+// you cannot swap fn and first like this, it gets an error
+// let {first:fn}
+// becuase first shit is not being returned by doWork() 
 
-// above is not building an obj literal
-// why object declaration then ?
-// assigning values that is returned from object thats why {} because right hand side
-// is returning an object
-console.log(fn,  twitter);  // srinivas srini88
+console.log(first,  twitter);  // srinivas srini88
