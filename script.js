535f4f51407ccfdc4863a8add485f76bdbf44339
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-01-04 12:04:07 -0500

    const has block scoping

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-const x = 20;  //no issues with const as well
-//this const x scope is different from the functional x scope
+if (true){
+  const x = 20;    //now const has block scoping
+}
 var doWork = function(){
   var x = 10;  //this x is within the function level scope
   return x;
@@ -8,4 +9,4 @@ var doWork = function(){
 //the x that is defined inside of doWork is hiding or shadows the x that is
 // defined outside of that fn
 console.log(doWork());  //10
-console.log(x);  //20
+console.log(x);  //x is not defined
