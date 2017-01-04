cbf809c5585d0b766fcf983504a2afd45ca4d2ac
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-01-04 12:44:19 -0500

    destructuring basics

--- script.js
+++ script.js
@@ -7,8 +7,19 @@
 
 
 let x = 3;
-var x = 2;  //script.js:10 Uncaught SyntaxError: Identifier 'x' has already been declared
+let y = 2;
+// right side is an array
+// destructing an array on the right side, give the variables x and y the values from array
 
+// not creating a new array on the left hand side , only dealing with variables
+// right hand side is an array
+// array built is with values y and x on rhs
 [x,y] = [y,x];
 
-console.log(x, y);
+console.log(x, y);  //2, 3 got swapped
+
+let [a, b] = [9,10];  //right side an array
+//first value put it in a , 2nd value in b
+// i'm not building an object literal below
+// {x,y}  // assignment statment to pick off specific properties of obj
+console.log(a,b);
