a9f85d0d6fb026832a74ba81ff5e8947677a2f83
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-01-04 10:51:17 -0500

    es6 function

---
+++ notes.js
@@ -0,0 +1,66 @@
+https://app.pluralsight.com/library/courses/javascript-fundamentals-es6/table-of-contents
+JS is implementation of ECMA script
+
+ES6 introduces new syntax for the language
+
+variables and fn parameters
+
+let will be new var, safer
+
+rest parameters, default parameters
+
+new class syntax in JS
+
+classes and new functional javascript
+
+arrow function - create a fn , no fn keyword syntax
+
+iterators, generator fns, tour of built in objects like set and map collections
+
+asynchronous programming with ES6 with promises
+
+now es6 has formalized an api
+
+after async programming, ES6 objects, new apis
+
+there is a new proxy objects
+
+JS has new modules, we need these days for modern js
+
+look at ES6 import and export syntax
+
+module about using ES6 today, use tools like tracer and babel , that transpile ES6 to ES5 code
+
+we will produce code that will run on all the devices.
+
+Tracer in a browser, will let u some of the ES6 features
+
+Tracer in a plunk , it is traceur 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,11 @@
+//console.log("Srini");
+
+var add = function (x, y){
+  return x+y
+}
+
+//ES6 implementation
+var add1 = (x,y) => x+y;
+console.log(add(4, 2));
+console.log(add1(5, 6));
+
