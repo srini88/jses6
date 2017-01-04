96d00b3962f1ec8ff74acf895525bb4e591afef8
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-01-04 11:48:25 -0500

    var vs let block level scoping for loop

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-//console.log("Srini");
-
-var add = function (x, y){
-  return x+y
-}
-
-//ES6 implementation
-// let is also working
-let add1 = (x,y) => x+y;
-//console.log(add(4, 2));
-//console.log(add1(5, 6));
-
-//example with var
 var doWork = function(flag){
-  if (flag){
-    let x = 4;  //x is only available to use inside the if block
+  for (var i=0; i<10; ++i){
   }
-  return x;
+  return i;
 }
 
-console.log(doWork(true));  //4 no surprise
-console.log(doWork(false));
-//Uncaught ReferenceError: x is not defined
-    // at doWork (script.js:18)
-    // at script.js:21
+console.log(doWork());  //10 //we get 10 because i is hoisted, it is not only
+//within the for which we wanted
 
-//moving forward use the let keyword instead of var keyword to avoid this
-//implicit variable hoisting
+var doWorkLet = function(flag){
+  for (let i=0; i<10; ++i){
+  }
+  return i;
+}
+
+console.log(doWorkLet());  //error - this i is constrained within for and we get
+//true block scoping
