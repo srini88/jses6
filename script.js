3f3dde98fe42c1bb47550baa01ebd54d7a1043e1
Palghat, Srinivas <Srinivas.PalghatViswanath@rsa.com>
2017-01-04 12:05:02 -0500

    var has no block scoping

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 if (true){
-  let x = 20;    //let has block scoping
+  var x = 20;    //var has no block scoping
 }
 var doWork = function(){
   var x = 10;  //this x is within the function level scope
@@ -9,4 +9,4 @@ var doWork = function(){
 //the x that is defined inside of doWork is hiding or shadows the x that is
 // defined outside of that fn
 console.log(doWork());  //10
-console.log(x);  //x is not defined
+console.log(x);  //20
